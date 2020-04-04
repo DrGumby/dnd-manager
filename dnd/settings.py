8cbde000e9a24affce1622b5139033d7e17d7dd7
Kamil Vojanec <xvojan00@stud.fit.vutbr.cz>
2020-04-04 18:42:04 +0200

    [FEATURE] Prepared for redux build

--- dnd/settings.py
+++ dnd/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'player.apps.PlayerConfig',
     'rest_framework',
+    'frontend',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
